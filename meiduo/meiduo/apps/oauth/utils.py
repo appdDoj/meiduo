490285eee256ed8f58780b20775a5df73896739d
杜建 <1035246868@qq.com>
2018-10-26 08:35:10 +0800

    QQ登录之面向封装开发思路介绍

---
+++ meiduo/meiduo/apps/oauth/utils.py
@@ -0,0 +1,18 @@
+
+
+
+class OAuthQQ(object):
+    """QQ登录的工具类：封装了QQ登录的部分过程"""
+
+    def get_login_url(self):
+        """提供QQ扫码登录页面的网址"""
+
+        # 准备url
+        url = 'https://graph.qq.com/oauth2.0/authorize'
+
+        # 准备参数
+
+        # url拼接参数
+
+        # 返回login_url
+        pass
--- meiduo/meiduo/apps/oauth/views.py
+++ meiduo/meiduo/apps/oauth/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework.response import Response
 
+from .utils import OAuthQQ
 # Create your views here.
 
 # url(r'^qq/authorization/$', views.QQAuthURLView.as_view()),
 class QQAuthURLView(APIView):
     """提供QQ登录页面网址"""
+
     def get(self, request):
-        pass
+
+        # 创建OAuthQQ对象
+        oauth = OAuthQQ()
+
+        # 获取QQ扫码登录页面的网址
+        login_url = oauth.get_login_url()
+
+        # 响应login_url
+        return Response({'login_url':login_url})
