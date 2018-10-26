5377c9b29fa0f294fa8c706e90e5989418c8110f
杜建 <1035246868@qq.com>
2018-10-26 19:06:15 +0800

    录入数据库商品数据

--- meiduo/script/import_areas_to_db.sh
+++ meiduo/script/import_areas_to_db.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-mysql -h数据库ip地址 -u数据库用户名 -p 数据库密码 < areas.sql
-例如：
-mysql -h192.168.103.132 -uroot -pmysql meiduo_mall_04 < areas.sql
-mysql -h127.0.0.1 -uroot -proot meiduo18 < areas.sql
-        'NAME': 'meiduo18',
-        'HOST': 'localhost',
-        'PORT': 3306,
-        'USER': 'meiduo_sz18',
-        'PASSWORD': 'meiduo',
-mysql  -uroot -p meiduo18  < areas.sql
-导出数据库指定的表
-mysqldump -uroot -p meiduo18 tb_areas  > tb_areas.sql
+
+mysql -h192.168.103.132 -uroot -proot meiduo18_ < goods_data.sql
