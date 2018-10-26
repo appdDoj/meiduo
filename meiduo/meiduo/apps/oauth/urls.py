fd42620683debadf8f0d45a662093a06b8518992
杜建 <1035246868@qq.com>
2018-10-26 09:01:26 +0800

    QQ登录之处理oauth_callback回调接口设计和定义

--- meiduo/meiduo/apps/oauth/urls.py
+++ meiduo/meiduo/apps/oauth/urls.py
@@ -6,4 +6,6 @@ from . import views
 urlpatterns = [
     # 提供用户登录到QQ的网址
     url(r'^qq/authorization/$', views.QQAuthURLView.as_view()),
+    # 处理oauth_callback回调，提取code,access_token,openid
+    url(r'^qq/user/$', views.QQAuthUserView.as_view()),
 ]
--- meiduo/meiduo/apps/oauth/views.py
+++ meiduo/meiduo/apps/oauth/views.py
@@ -5,6 +5,26 @@ from rest_framework.response import Response
 from .utils import OAuthQQ
 # Create your views here.
 
+
+# url(r'^qq/user/$', views.QQAuthUserView.as_view()), 
+class QQAuthUserView(APIView):
+    """
+    处理oauth_callback回调页面时
+    提取code,access_token,openid
+    """
+
+    def get(self, request):
+        # 提取code请求参数
+        # 使用code向QQ服务器请求access_token
+        # 使用access_token向QQ服务器请求openid
+        # 使用openid查询该QQ用户是否在美多商城中绑定过用户
+
+
+        # 如果openid已绑定美多商城用户，直接生成JWT token，并返回
+        # 如果openid没绑定美多商城用户，创建用户并绑定到openid
+        pass
+
+
 # url(r'^qq/authorization/$', views.QQAuthURLView.as_view()),
 class QQAuthURLView(APIView):
     """提供QQ登录页面网址"""
