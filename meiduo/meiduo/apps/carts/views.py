603327c04f9be0f4c4705927579c883a30e3f788
杜建 <1035246868@qq.com>
2018-10-27 08:18:18 +0800

    购物车之取消购物车视图用户认证

--- meiduo/meiduo/apps/carts/views.py
+++ meiduo/meiduo/apps/carts/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 
+
 # Create your views here.
 
 
 class CartView(APIView):
-    """购物车增、查、改、删"""
+    """购物车增、查、改、删
+
+    用户不登录可以访问该接口：不传入JWT token，不指定权限
+    用户登录也可以访问该接口：必须传入JWT token，必须指定权限
+
+    用户登录和未登录都可以访问该接口：
+        不能指定权限，保证未登录用户可以访问
+        必须传入JWT token，保证登录用户可以是被身份
+    """
+
+    def perform_authentication(self, request):
+        """重写执行认证方法的目的：取消视图的认证，是为了保证用户登录或未登录都可以进入到视图内部
+        可以避免用户未登录，但是传入了JWT token时，在认证过程中报401的错误
+        """
+        pass
 
     def post(self, request):
         """添加购物车"""
