1b9a4defe2e38cd05989de17440a225686c790b4
杜建 <1035246868@qq.com>
2018-10-27 10:31:02 +0800

    订单之提交订单保存OrderInfo订单基本信息

--- meiduo/meiduo/apps/orders/serializers.py
+++ meiduo/meiduo/apps/orders/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from django.utils import timezone
+from decimal import Decimal
 
 from goods.models import SKU
 from . models import OrderInfo
@@ -26,10 +28,32 @@ class CommitOrderSerializer(serializers.ModelSerializer):
         """重写序列化器的create方法：
         为了能够自己写代码按照需求保存订单基本信息和订单商品信息
         """
-
         # 获取当前保存订单时需要的信息
 
+        # 获取登录用户
+        user = self.context['request'].user
+
+        # 生成订单编号：20180802172710000000001 (年月日时分秒+用户id),用户id最多9位，不够9位补0
+        # 获取当前时区的当前的时间 ： timezone.now()
+        order_id = timezone.now().strftime() + ('%09d' % user.id)
+
+        # 获取validated_data里面的address和pay_method
+        address = validated_data.get('address')
+        pay_method = validated_data.get('pay_method')
+
         # 保存订单基本信息 OrderInfo（主体业务逻辑）
+        # 模型类.objects.create() : 在创建模型对象的同时给属性赋值，并自动同步数据到数据库
+        order = OrderInfo.objects.create(
+            order_id=order_id,
+            user = user,
+            address = address,
+            total_count = 0,
+            total_amount = Decimal('0.00'),
+            freight = Decimal('10.00'),
+            pay_method = pay_method,  # 1(货到付款) / 2(支付宝支付)
+            status = OrderInfo.ORDER_STATUS_ENUM['UNPAID'] if pay_method == OrderInfo.PAY_METHODS_ENUM[
+            'ALIPAY'] else OrderInfo.ORDER_STATUS_ENUM['UNSEND']
+        )
 
         # 从redis读取购物车中被勾选的商品信息
 
