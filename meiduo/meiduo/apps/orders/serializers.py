0b29828e10241f8b4d9cbf3e33907f5eff127bc5
杜建 <1035246868@qq.com>
2018-10-27 10:29:03 +0800

    订单之提交订单序列化器准备

--- meiduo/meiduo/apps/orders/serializers.py
+++ meiduo/meiduo/apps/orders/serializers.py
@@ -1,11 +1,34 @@
 from rest_framework import serializers
 
 from goods.models import SKU
+from . models import OrderInfo
 
 
 class CommitOrderSerializer(serializers.ModelSerializer):
     """提交订单序列化器"""
-    pass
+
+    class Meta:
+        model = OrderInfo
+        # order_id ：输出；address 和 pay_method : 输入
+        fields = ('order_id', 'address', 'pay_method')
+        read_only_fields = ('order_id',)
+        # 指定address 和 pay_method 为输出
+        extra_kwargs = {
+            'address': {
+                'write_only': True,
+            },
+            'pay_method': {
+                'write_only': True,
+            }
+        }
+
+    def create(self, validated_data):
+        """重写序列化器的create方法：
+        为了能够自己写代码按照需求保存订单基本信息和订单商品信息
+        """
+
+        # 返回新建的资源对象
+        return 'OrderInfo()'
 
 
 class CartSKUSerializer(serializers.ModelSerializer):
